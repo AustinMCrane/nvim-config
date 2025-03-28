a7ad1a3b2614a41120ab8d14e00cae648e2b861e
Austin Crane <austin.crane@fhr.com>
2025-03-28 09:43:04 -0500

    feat: updated with my initial plugins

---
+++ lua/custom/init.lua
@@ -0,0 +1,5 @@
+require 'custom.set'
+require 'custom.remap'
+require 'custom.setup'
+
+vim.print 'Custom Neovim configuration loaded successfully!' -- Print a message to indicate successful loading
