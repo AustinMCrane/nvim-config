7255aa2b90cfaf7644530e1b08fb4a2ffd3b9ebc
Austin Crane <austin.crane@fhr.com>
2025-04-11 09:11:16 -0500

    fix: custom plugins and ts_ls biome enabled
    - ts_ls and biome enabled for language servers
    - moved out the github copilot plugin from init.lua to its own file in
      custom plugins

--- init.lua
+++ init.lua
@@ -679,7 +679,10 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
+        biome = {},
+
+        --
         --
 
         lua_ls = {
