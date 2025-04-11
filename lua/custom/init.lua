ea9b323bbf25632fb1b37df8c79397f1315a60bd
Austin Crane <austin.crane@fhr.com>
2025-04-11 10:24:14 -0500

    fix: fixed mappings that were replaced from pulling in root

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,5 +1,2 @@
 require 'custom.set'
 require 'custom.remap'
-require 'custom.setup'
-
-vim.print 'Custom Neovim configuration loaded successfully!' -- Print a message to indicate successful loading
--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -6,3 +6,8 @@ vim.api.nvim_set_keymap(
   '<cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<cr>',
   { silent = true }
 )
+
+-- Telescope
+vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
+vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations, { desc = '[G]oto [I]mplementation' })
+vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]Definition' })
