41d0ce659047d6b1c165cd11a7ea2b040447e855
Austin Crane <acrane@bellese.io>
2025-08-10 14:34:04 -0500

    fix: mcphub stuff working

--- lua/custom/setup.lua
+++ lua/custom/setup.lua
@@ -11,3 +11,18 @@ require('codecompanion').setup {
     },
   },
 }
+
+print(os.getenv 'GITHUB_TOKEN')
+
+require('mcphub').setup {
+  global_env = {
+    ['input:github_token'] = os.getenv 'GITHUB_TOKEN' or '',
+  },
+  workspace = {
+    enabled = true, -- Default: true
+    look_for = { '.vscode/mcp.json' },
+    reload_on_dir_changed = true, -- Auto-switch on directory change
+    port_range = { min = 40000, max = 41000 }, -- Port range for workspace hubs
+    get_port = nil, -- Optional function for custom port assignment
+  },
+}
