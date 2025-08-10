7d13d06cb9eb1dbbfaf65d6c31b5909103f1c24b
Austin Crane <acrane@bellese.io>
2025-08-10 07:35:11 -0500

    feat(mcp): added mcphub and codecompanion instead of copilotchat

---
+++ lua/custom/plugins/codecompanion.lua
@@ -0,0 +1,15 @@
+return {
+  'olimorris/codecompanion.nvim',
+  opts = {
+    strategies = {
+      chat = {
+        adapter = 'copilot',
+      },
+    },
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'ravitemer/mcphub.nvim',
+  },
+}
--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -11,3 +11,6 @@ vim.api.nvim_set_keymap(
 vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
 vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations, { desc = '[G]oto [I]mplementation' })
 vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]Definition' })
+
+vim.api.nvim_set_keymap('n', 'nn', '<cmd>lua require("codecompanion").chat()<cr>', { desc = '[C]ode [N]ew Chat' })
+-- vim.cmd.command('Chat', 'lua require("codecompanion").chat()')
